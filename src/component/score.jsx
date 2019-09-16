daa9720fecc962510579fb460d43c798a4abd40f
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-09-16 09:43:27 +0200

    highscore border

--- src/component/score.jsx
+++ src/component/score.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 class Score extends React.Component {
-
+    
     constructor(props) {
         super(props)
         this.state = {
             high_score: localStorage.getItem('highscore')
         }
     }
+
+    share_button() {
+        console.log("Hey je wilt een tweet maken")
+    }
+
     render() {
         return (
-            <div id="Score" className="d-flex justify-content-center">
-                <div><p id="klik_seconde"><span id="kliks_seconde"></span> kliks per seconde </p>
+            <div id="Scoren" className="d-flex justify-content-center">
+                <div className="border border-primary"><p id="klik_seconde"><span id="kliks_seconde"></span> kliks per seconde </p>
                 <p>High Score: <span id="score">{this.state.high_score}</span></p>
                 </div>
             </div>
