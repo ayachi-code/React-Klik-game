cddff040e83a0fb1c0dac065943631de25f91b7e
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-09-16 13:05:03 +0200

    Table concept gemaalt

--- src/component/leaderbord.jsx
+++ src/component/leaderbord.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 class Leaderbord extends React.Component {
     render() {
         return(
             <div id="leaderbord">
-                <p>Leaderbord:</p>
+                <table className="border border-primary">
+                <thead className="border border-primary">
+                    <tr>
+                        <th>Naam</th>
+                        <th>&nbsp; Highscore</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Bilal</th>
+                        <th>&nbsp; 120</th>
+                    </tr>
+                </tbody>
+                    </table>
                 </div>
         );
     }
