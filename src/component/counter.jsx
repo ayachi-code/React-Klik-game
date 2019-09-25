8880d638807f510ffa39174685f1d13769230d71
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-09-25 10:42:32 +0200

    Size vergroot

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -107,7 +107,7 @@ class Counter extends React.Component {
     render() {
        return(
           <div>
-            <p className="float-right h1">{this.state.counter}</p>
+            <p className="display-1 float-right h1">{this.state.counter}</p>
             <button id="start" className="rounded-pill w-25 mt-3 btn btn-primary active" onClick={this.button}>Start</button>
             <br/>
             <button id="delen" className="rounded-pill w-25 mt-3 btn btn-primary active" onClick={this.share_button}>High score delen</button>
